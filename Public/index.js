0ec0f7f3f8d605a4e79cf8d8b9f71416e165b9ec
Ashish Kumar <ashishhota79@gmail.com>
2021-03-24 23:35:47 +0530

    hopefully this works

---
+++ Public/index.js
@@ -0,0 +1,12 @@
+$(".fa-github").click(function(){
+    $(location).attr('href','https://github.com/subashmanohari');
+});
+
+
+$(".fa-linkdin").click(function(){
+    $(location).attr('href','https://www.linkedin.com/in/subashmanohari/');
+});
+
+$(".fa-laptop-code").click(function(){
+    $(location).attr('href','https://codeforces.com/profile/subashmanohari');
+});
--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const express = require('express');
 var fs = require('fs');
 var path = require('path');
 
-const PORT=8080;
+const port=8080;
 
 const app = express();
 
 
+app.listen(port, function() {
+  console.log('app started at port : ' + port);
+});
+
+
+app.use(express.static(__dirname + '/Public'));
+
+
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + 'index.html');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
